7570aee0314ac8fd8514ec6e4a5ef074f8f589b8
Giovanni Curiel dos Santos <gcuriel@cpqd.com.br>
2018-01-29 14:35:01 -0200

    Fixing conf.py - adding custom css in the correct way

--- docs/conf.py
+++ docs/conf.py
@@ -98,11 +98,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/css/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
 
 # -- Options for HTMLHelp output ------------------------------------------
 
