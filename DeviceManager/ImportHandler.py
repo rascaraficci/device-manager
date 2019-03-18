33cde5477bde8f37bcc27f5ff2fe9235199e5cd1
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-03-18 11:16:08 -0300

    kafka notification

--- DeviceManager/ImportHandler.py
+++ DeviceManager/ImportHandler.py
@@ -62,57 +62,127 @@ class ImportHandler:
 
     @staticmethod
     def drop_sequences():
-        LOGGER.info(f"dropping sequences...") 
-
         db.session.execute("DROP SEQUENCE template_id")
-        LOGGER.info(f"template_id sequence dropped")
-
         db.session.execute("DROP SEQUENCE attr_id")
-        LOGGER.info(f"attr_id sequence dropped")
-
-        # db.session.execute("DROP SEQUENCE override_id")
-        # LOGGER.info(f"override_id sequence dropped")
+        LOGGER.info(f" Removed sequences") 
 
     @staticmethod
     def restore_sequences():
-        LOGGER.info(f"creating sequences...") 
-
         max_template_id = db.session.query(func.max(DeviceTemplate.id)).scalar() + 1
         db.session.execute("CREATE SEQUENCE template_id START {}".format(str(max_template_id)))
-        LOGGER.info(f"template_id sequence created")
 
         max_attr_id = db.session.query(func.max(DeviceAttr.id)).scalar() + 1
         db.session.execute("CREATE SEQUENCE attr_id START {}".format(str(max_attr_id)))
-        LOGGER.info(f"attr_id sequence created")    
+
+        LOGGER.info(f" Restored sequences") 
 
     @staticmethod
-    def delete_records():
-        LOGGER.info(f"deleting records...") 
-
+    def delete_records(tenant):
         overrides = db.session.query(DeviceOverride)
         for override in overrides:
             db.session.delete(override)
-        LOGGER.info(f"deleted overrides")
+        LOGGER.info(f" Deleted overrides")
 
         devices = db.session.query(Device)
         for device in devices:
+            data = serialize_full_device(device, tenant)
+            kafka_handler = KafkaHandler()
+            kafka_handler.remove(data, meta={"service": tenant})
+            if CONFIG.orion:
+                subscription_handler = PersistenceHandler(service=tenant)
+                subscription_handler.remove(device.persistence)
             db.session.delete(device)
-        LOGGER.info(f"deleted devices") 
+        LOGGER.info(f" Deleted devices") 
 
         templates = db.session.query(DeviceTemplate)
         for template in templates:
             db.session.delete(template)
-        LOGGER.info(f"deleted templates")    
+        LOGGER.info(f" Deleted templates")    
 
     @staticmethod
-    def clear_db_config():
+    def clear_db_config(tenant):
         ImportHandler.drop_sequences()
-        ImportHandler.delete_records()
+        ImportHandler.delete_records(tenant)
         db.session.flush()
 
     @staticmethod
     def restore_db_config():
         ImportHandler.restore_sequences()
+
+    @staticmethod
+    def save_templates(json_data, json_payload):
+        saved_templates = []
+        for template in json_data['templates']:
+            loaded_template = DeviceTemplate(**template)
+            for json in json_payload['templates']:
+                if(json['id'] == template["id"]):
+                    load_attrs(json['attrs'], loaded_template, DeviceAttr, db)
+            db.session.add(loaded_template)
+            saved_templates.append(loaded_template) 
+
+        LOGGER.info(f" Saved templates")     
+        return saved_templates    
+
+
+    @staticmethod
+    def save_devices(json_data, json_payload, saved_templates):
+        saved_devices = []
+        for device in json_data['devices']:
+            device.pop('templates', None)
+            loaded_device = Device(**device)
+            for json in json_payload['devices']:
+                if(json['id'] == device["id"]):
+                    loaded_device.templates = []
+
+                    for template_id in json.get('templates', []):
+                        for saved_template in saved_templates:
+                            if(template_id == saved_template.id):
+                                loaded_device.templates.append(saved_template)
+
+                    auto_create_template(json, loaded_device)
+
+            db.session.add(loaded_device)
+            saved_devices.append(loaded_device) 
+        LOGGER.info(f" Saved devices")        
+        return saved_devices    
+
+    def notify_kafka(saved_devices, tenant):
+        kafka_handler = KafkaHandler()
+        if CONFIG.orion:
+            ctx_broker_handler = OrionHandler(service=tenant)
+            subs_handler = PersistenceHandler(service=tenant)
+        else:
+            ctx_broker_handler = None
+            subs_handler = None
+
+        full_device = None
+        orm_devices = []     
+
+
+        devices = []
+        for orm_device in saved_devices:
+            devices.append(
+                {
+                    'id': orm_device.id,
+                    'label': orm_device.label
+                }
+            )
+
+            full_device = serialize_full_device(orm_device, tenant)
+
+            # Updating handlers
+            kafka_handler.create(full_device, meta={"service": tenant})
+            if CONFIG.orion:
+                # Generating 'device type' field for history
+                type_descr = "template"
+                for dev_type in full_device['attrs'].keys():
+                    type_descr += "_" + str(dev_type)
+                # TODO remove this in favor of kafka as data broker....
+                ctx_broker_handler.create(full_device, type_descr)
+                sub_id = subs_handler.create(full_device['id'], type_descr)
+                orm_device.persistence = sub_id
+
+
 
     @staticmethod
     def import_data(req):
@@ -127,89 +197,29 @@ class ImportHandler:
         violated. This might happen if two attributes have the same name, for
         instance.
         """
-        LOGGER.info(f"starting importing data...")
-        
-        init_tenant_context(req, db)
-        tpl, json_payload = parse_payload(req, import_schema)
-
-        ImportHandler.clear_db_config()
-
-        saved_templates = []
-        for template in tpl['templates']:
-            loaded_template = DeviceTemplate(**template)
-            for json in json_payload['templates']:
-                if(json['id'] == template["id"]):
-                    load_attrs(json['attrs'], loaded_template, DeviceAttr, db)
-            db.session.add(loaded_template)
-            saved_templates.append(loaded_template)
-
-
-        # Handlers
-        kafka_handler = KafkaHandler()
-        if CONFIG.orion:
-            ctx_broker_handler = OrionHandler(service=tenant)
-            subs_handler = PersistenceHandler(service=tenant)
-        else:
-            ctx_broker_handler = None
-            subs_handler = None
-
-        full_device = None
-        orm_devices = []
-
-        saved_devices = []
-        for device in tpl['devices']:
-            device.pop('templates', None)
-            loaded_device = Device(**device)
-            for json in json_payload['devices']:
-                if(json['id'] == device["id"]):
-                    loaded_device.templates = []
-
-                    for template_id in json.get('templates', []):
-                        for saved_template in saved_templates:
-                            if(template_id == saved_template.id):
-                                loaded_device.templates.append(saved_template)
-
-                    auto_create_template(json, loaded_device)
-            db.session.add(loaded_device)
-            saved_devices.append(loaded_device)
-
-        ImportHandler.restore_db_config()
-
 
         try:
+            tenant = init_tenant_context(req, db)
+
+            ImportHandler.clear_db_config(tenant)
+
+            json_data, json_payload = parse_payload(req, import_schema)
+
+            saved_templates = ImportHandler.save_templates(json_data, json_payload)
+            saved_devices = ImportHandler.save_devices(json_data, json_payload, saved_templates)
+
+            ImportHandler.restore_db_config()
+
             db.session.commit()
 
-            # devices = []
-            # for orm_device in saved_devices:
-            #     devices.append(
-            #         {
-            #             'id': orm_device.id,
-            #             'label': orm_device.label
-            #         }
-            #     )
-
-            #     full_device = serialize_full_device(orm_device, tenant)
-
-                # # Updating handlers
-                # kafka_handler.create(full_device, meta={"service": tenant})
-                # if CONFIG.orion:
-                #     # Generating 'device type' field for history
-                #     type_descr = "template"
-                #     for dev_type in full_device['attrs'].keys():
-                #         type_descr += "_" + str(dev_type)
-                #     # TODO remove this in favor of kafka as data broker....
-                #     ctx_broker_handler.create(full_device, type_descr)
-                #     sub_id = subs_handler.create(full_device['id'], type_descr)
-                #     orm_device.persistence = sub_id
-
-            LOGGER.info(f"Imported data into the database")
+            ImportHandler.notify_kafka(saved_devices, tenant)
 
         except IntegrityError as e:
             LOGGER.error(f' {e}')
             raise HTTPRequestError(400, 'Template attribute constraints are violated by the request')
-        except:
-            LOGGER.error(f'roolbacking')
-            db.session.rollback()    
+        # except NameError as e:
+        #     LOGGER.error(f' {e}')
+        #     db.session.rollback()    
         finally:
             db.session.close()
 
@@ -223,9 +233,11 @@ class ImportHandler:
 @importing.route('/import', methods=['POST'])
 def flask_import_data():
     try:
+        LOGGER.info(f" Starting importing data...")
+
         result = ImportHandler.import_data(request)
 
-        LOGGER.info(f"Importing data")
+        LOGGER.info(f" Imported data!")
 
         return make_response(jsonify(result), 200)
 
