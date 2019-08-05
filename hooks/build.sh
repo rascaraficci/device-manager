8ef268b665658cdf3860b342098f2f4a57d00905
kevin <kevin@cpqd.com.br>
2019-08-05 13:03:04 -0300

    Revert "Teste de build travis"
    This reverts commit dd200b77ee0be4130dc240364465976969b7c466.

--- hooks/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-docker build -t dojot/device-manager ../
-docker build -t dredd/test -f tests/Dockerfile ../
-docker-compose -p test -f ../tests/docker-compose.yaml up -d kafka data-broker postgres device-manager device-manager-redis postgres-users
-docker-compose -p test -f ../tests/docker-compose.yaml run --rm test-runne
